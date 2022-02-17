00d15293bdef11bf73f154cae02819919180c25b
EnzoFB <enzo.ferreirabastos@gmail.com>
2022-02-17 16:59:58 +0100

    Réagissez aux évenements

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -12,6 +12,7 @@ export class FaceSnapComponent implements OnInit {
   createdDate!: Date;
   snaps!: number;
   imageUrl!: string;
+  buttonText!: string;
 
   ngOnInit() {
       this.title = 'Archibald';
@@ -19,6 +20,16 @@ export class FaceSnapComponent implements OnInit {
       this.createdDate = new Date();
       this.snaps = 6;
       this.imageUrl = 'https://media-cdn.tripadvisor.com/media/photo-s/1a/02/50/bb/troupial-the-national.jpg';
+      this.buttonText = 'Oh Snap!';
   }
 
+  onSnap() {
+    if (this.buttonText === 'Oh Snap!') { 
+      this.snaps++;
+      this.buttonText = 'Oups unSnap!';
+    } else {
+      this.snaps--;
+      this.buttonText = 'Oh Snap!';
+    }
+  }
 }
