88613ac12c23d2cf5e4f12c67c25b64356b8b065
EnzoFB <enzo.ferreirabastos@gmail.com>
2022-03-04 14:02:11 +0100

    Partagez des données avec les services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { registerLocaleData } from '@angular/common';
-import * as fr from '@angular/common/locales/fr'
+import * as fr from '@angular/common/locales/fr';
+import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { HeaderComponent } from './header/header.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    FaceSnapComponent
+    FaceSnapComponent,
+    FaceSnapListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
