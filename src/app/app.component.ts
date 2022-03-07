d99936850cbfcfb3240a2aa9b3e2ec7ffa4814fa
EnzoFB <enzo.ferreirabastos@gmail.com>
2022-03-07 14:18:45 +0100

    Manipulez les émissions avec les opérateurs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, Observable } from 'rxjs';
+import { filter, interval, map, Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +9,20 @@ import { interval, Observable } from 'rxjs';
 
 export class AppComponent implements OnInit {
 
-  interval$!: Observable<number>;
+  interval$!: Observable<string>;
 
   ngOnInit() {
-
-    this.interval$ = interval(1000)
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3 === 0),
+      map(value => value % 2 === 0 ?
+        `${value} est pair !` : 
+        `${value} est impair !`
+      ), 
+      tap(text => this.logger(text))
+    );
   }
 
+  logger(text: string) {
+    console.log(`log: ${text}`);
+  }
 }
