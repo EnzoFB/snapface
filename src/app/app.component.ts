354862b61f947cf53d6fd7165b9a743daea4ec1c
EnzoFB <enzo.ferreirabastos@gmail.com>
2022-03-07 14:40:52 +0100

    Passez d'un Observable à un autre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { filter, interval, map, Observable, tap } from 'rxjs';
+import { concatMap, delay, exhaustMap, filter, interval, map, mergeMap, Observable, of, switchMap, take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +11,9 @@ export class AppComponent implements OnInit {
 
   interval$!: Observable<string>;
 
+  redTrainsCalled = 0;
+  yellowTrainsCalled = 0;
+
   ngOnInit() {
     this.interval$ = interval(1000).pipe(
       filter(value => value % 3 === 0),
@@ -18,11 +21,33 @@ export class AppComponent implements OnInit {
         `${value} est pair !` : 
         `${value} est impair !`
       ), 
-      tap(text => this.logger(text))
+      // tap(text => this.logger(text))
+    );
+
+    interval(500).pipe(
+      take(10),
+      map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
+      tap(color => console.log(`La lumière s'allume en ${color}`, `${this.translateColor(color)}`)),
+      switchMap(color => this.getTrainObservable$(color)),
+      tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+    ).subscribe();
+  }
+
+  getTrainObservable$(color: 'rouge' | 'jaune') {
+    const isRedTrain = color === 'rouge';
+    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+    console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+    return of({ color, trainIndex}).pipe(
+      delay(isRedTrain ? 5000 : 6000)
     );
   }
 
-  logger(text: string) {
-    console.log(`log: ${text}`);
+  translateColor(color: 'rouge' | 'jaune') {
+    return color === 'rouge' ? 'red' : 'yellow';
   }
+
+  // logger(text: string) {
+  //   console.log(`log: ${text}`);
+  // }
 }
