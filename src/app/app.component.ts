35fb05de0fb2440c42b2a6d815dd49e01e2c3b98
EnzoFB <enzo.ferreirabastos@gmail.com>
2022-03-07 13:32:22 +0100

    Créez un Observable et souscrivez-y

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,11 @@ import { Component, OnInit } from '@angular/core';
 
 export class AppComponent implements OnInit {
 
-  ngOnInit() { }
+  interval$!: Observable<number>;
+
+  ngOnInit() {
+
+    this.interval$ = interval(1000)
+  }
 
 }
