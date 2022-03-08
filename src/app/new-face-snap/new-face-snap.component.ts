f4a431914fc174be06509c4a3964909c679881ab
EnzoFB <enzo.ferreirabastos@gmail.com>
2022-03-08 14:45:12 +0100

    Maintenez l'intégrité des données avec Validators

--- src/app/new-face-snap/new-face-snap.component.ts
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { map, Observable } from 'rxjs';
 import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapService } from '../services/face-snaps-service';
 
 @Component({
   selector: 'app-new-face-snap',
@@ -13,15 +15,20 @@ export class NewFaceSnapComponent implements OnInit {
 
   snapForm!: FormGroup;
   faceSnapPreview$!: Observable<FaceSnap>;
+  urlRegex!: RegExp;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+              private faceSnapsService: FaceSnapService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.snapForm = this.formBuilder.group({
-      title: [null],
-      description: [null],
-      imageUrl: [null],
+      title: [null, [Validators.required]],
+      description: [null, [Validators.required]],
+      imageUrl: [null, [Validators.required, Validators.pattern(this.urlRegex)]],
       location: [null]
+    }, {
+      updateOn: 'blur'
     });
     this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
       map(formValue => ({
@@ -31,10 +38,12 @@ export class NewFaceSnapComponent implements OnInit {
         id: 0
       }))
     );
+    this.urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
   }
 
   onSubmitForm() {
-    console.log(this.snapForm.value)
+    this.faceSnapsService.addFaceSnap(this.snapForm.value);
+    this.router.navigateByUrl('/facesnaps');
   }
 
 }
